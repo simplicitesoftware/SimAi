241771036d2bd83405ca4f76ae143937fe1d3919
designer (Designer) <>
2025-05-12 15:56:37 +0200

    Updated

--- resources/Disposition/default/SaiTools.js
+++ resources/Disposition/default/SaiTools.js
@@ -1,3 +1,72 @@
 var SaiTools = SaiTools || (function(param) {
+	const apiUrl = '/api/';
+	const endpointUrl = 'ext/SaiCreateModuleApi';
+	const user = 'AIUserAPI';
+	const password = 'Ec0Kj8Yb';
+	let token = '';
 	
+	async function loginApi(){
+		const response = await fetch(apiUrl + "login", {
+			method: 'GET',
+			headers: {
+				'accept': 'application/json',
+				'Authorization': $app._callAuth(user,password)
+			}
+		});
+		const data = await response.json();
+		this.token = data.authtoken;
+	}
+	async function callApi(data = {}){
+		const response = await fetch(apiUrl + endpointUrl, {
+			method: 'POST',
+			headers: {
+				'accept': 'application/json',
+				'Authorization': `Bearer ${this.token}`,
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		});
+		const res = await response.json();
+		return res;
+	}
+	async function getModuleInfo(data = {}){
+		const response = await fetch(this.apiUrl + this.endpointUrl + '?action=moduleInfo', {
+			method: 'GET',
+			headers: {
+				'accept': 'application/json',
+				'Authorization': `Bearer ${this.token}`
+			}
+		});
+		const res = await response.json();
+		return res;
+	}
+	async function getRedirectScope(data = {}){
+		const response = await fetch(this.apiUrl + this.endpointUrl + '?action=getRedirectScope', {
+			method: 'GET',
+			headers: {
+				'accept': 'application/json',
+				'Authorization': `Bearer ${this.token}`
+			}
+		});
+		const res = await response.json();
+		return res;
+	}
+	async function logoutApi() {
+		const response = await fetch(this.apiUrl + "logout", {
+			method: 'GET',
+			headers: {
+				'accept': 'application/json',
+				'Authorization': `Bearer ${this.token}`
+			}
+		});
+		const data = await response.json();
+		return data.status == '200';
+	}
+	return {
+		loginApi,
+		callApi,
+		getModuleInfo,
+		getRedirectScope,
+		logoutApi
+	}
 })();
--- resources/ObjectExternal/SaiNewModuleFront/CLASS.js
+++ resources/ObjectExternal/SaiNewModuleFront/CLASS.js
@@ -1,5 +1,21 @@
 Simplicite.UI.ExternalObjects.SaiNewModuleFront = class extends Simplicite.UI.ExternalObject {
 	async render(params, data = {}) {
-		$('#sainewmodulefront').append('Hello world!');
+		console.log('render');
+		await $ui.loadScript({url: $ui.getApp().dispositionResourceURL("SaiTools", "JS"),onload: function(){ SaiTools.loginApi();}});
+		window.addEventListener('beforeunload', function() {
+			SaiTools.logoutApi();
+		});
+		setModuleNameForm($('#sainewmodulefront'));
+		
 	}
+	async setModuleNameForm(ctn){
+		$(ctn).append('<input type="text" id="moduleName" placeholder="Entrez le nom du module" />');
+		$(ctn).append('<button id="createModule" onclick="SaiNewModuleFront.createModule()">Créer le module</button>');
+	}
+	async createModule(){
+		const moduleName = $('#moduleName').val();
+		const moduleInfo = await SaiTools.callApi({action: 'create', moduleName: moduleName});
+		console.log(moduleInfo);
+	}
+		
 };
