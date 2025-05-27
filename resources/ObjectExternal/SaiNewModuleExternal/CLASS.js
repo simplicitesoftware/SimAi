135bf8023215fbfce64e4a6eff1557a4c8f5153f
designer (Designer) <>
2025-05-27 09:33:42 +0200

    fix

--- resources/ObjectExternal/SaiNewModuleExternal/CLASS.js
+++
@@ -1,5 +0,0 @@
-Simplicite.UI.ExternalObjects.SaiNewModuleExternal = class extends Simplicite.UI.ExternalObject {
-	async render(params, data = {}) {
-		$('#sainewmoduleexternal').append('Hello world!');
-	}
-};
